19f3545918f83d27c019c43ef393175fadb85b68
eSint <haesena@gmail.com>
2017-01-26 15:00:55 +0100

    Added event-loading, event detail view, mini calendar

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `
-<h1>Hello {{name}}</h1>
-<dhtmlx-scheduler></dhtmlx-scheduler>
-`,
+  template: `<dhtmlx-scheduler></dhtmlx-scheduler>`,
 })
 export class AppComponent  { name = 'Scheduler'; }
