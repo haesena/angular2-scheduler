bb6d4b0bbbd9a9d37e2da8b44ce039869914affe
haesena <haesena@gmail.com>
2017-01-26 22:48:03 +0100

    Working on UI-Interface, experimenting with animations

--- app/scheduler/scheduler.component.ts
+++ app/scheduler/scheduler.component.ts
@@ -2,17 +2,38 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { SchedulerHttpService } from './scheduler.httpservice';
 
+import {
+    trigger,
+    state,
+    style,
+    transition,
+    animate
+} from '@angular/core';
+
 @Component({
     moduleId: module.id,
     selector: 'dhtmlx-scheduler',
     templateUrl: 'scheduler.component.html',
-    styleUrls: ['./scheduler.component.css']
+    styleUrls: ['./scheduler.component.css'],
+    animations: [
+        trigger('miniCalState', [
+            state('inactive', style({
+                transform: 'translateY(-100%)'
+            })),
+            state('active',   style({
+                transform: 'translateY(0)'
+            })),
+            transition('inactive => active', animate('100ms ease-in')),
+            transition('active => inactive', animate('100ms ease-out'))
+        ])
+    ]
 })
 
 export class SchedulerComponent implements OnInit {
 
     private events: any;
     private miniCalendar: any;
+    miniCalState: string;
 
     @Input() initialDate: Date = new Date();
     @Input() mode : string = "day";
@@ -32,6 +53,7 @@ export class SchedulerComponent implements OnInit {
         scheduler.config.readonly = true;
         scheduler.config.details_on_dblclick = true;
         scheduler.config.touch = "force";
+        scheduler.config.scroll_hour = new Date().getHours();
 
         scheduler.init('ng_scheduler', this.initialDate, this.mode);
 
@@ -43,11 +65,19 @@ export class SchedulerComponent implements OnInit {
         scheduler.attachEvent("onClick", function (id: string, e: Event){
             var ev = scheduler.getEvent(id);
             this.setEvent(ev);
+
+            if(this.showMiniCalendar)
+                this.toggleCalendar();
+
             return true;
         }.bind(this));
 
         scheduler.attachEvent("onEmptyClick", function (d: Date, e: Event){
             this.setEvent(null);
+
+            if(this.showMiniCalendar)
+                this.toggleCalendar();
+
             return true;
         }.bind(this));
 
@@ -56,7 +86,7 @@ export class SchedulerComponent implements OnInit {
             navigation:true,
             handler:function(date){
                 scheduler.setCurrentView(date, scheduler._mode);
-                this.showCalendar();
+                this.toggleCalendar();
             }.bind(this)
         });
 
@@ -82,13 +112,17 @@ export class SchedulerComponent implements OnInit {
             .catch(error => console.log(error));
     }
 
-    showCalendar() {
-        this.activeEvent = null;
+    toggleCalendar() {
         this.showMiniCalendar = !this.showMiniCalendar;
         setTimeout(function() {
             scheduler.updateCalendar(this.miniCalendar, scheduler._date);
         }.bind(this), 0);
 
-        console.log(this.showMiniCalendar);
+        if(this.showMiniCalendar) {
+            this.activeEvent = null;
+            this.miniCalState = 'active';
+        } else {
+            this.miniCalState = 'inactive';
+        }
     }
 }
